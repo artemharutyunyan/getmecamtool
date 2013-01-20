ece4a66186331403b999b2fd976cc12efdc4aaea
Sergey Shekyan <shekyan@gmail.com>
2013-01-20 01:34:22 -0800

    incremental checkin

--- src/disassemble.c
+++ src/disassemble.c
@@ -13,17 +13,113 @@
 
 #include "disassemble.h"
 
+#define MAX_FILE_NAME 1024
+#define MAX_FILE_SIZE 1048576
+#define DEFAULT_PATH "./"
+#define WEBUI_MAGIC 1141676733
+
 static void info() {
   printf("Try \'disassemble -h\' for more information\n"); //, PACKAGE);
 }
 
-int check_header(FILE *f)
+int calc_checksum(FILE* f)
 {
   return 1;
 }
-
-int extract_files(FILE *f)
+int check_header(FILE *f)
 {
+  int checksum = 0,
+      declared_checksum = 0,
+      declared_len = 0,
+      declared_magic = 0,
+      version = 0;
+  int retval = 1;
+
+  fseek(f, 0, SEEK_END); 
+  int file_len = ftell(f); // real file length
+
+  rewind(f);
+
+  fread(&declared_magic, 1, 4, f); // magic number
+  if(declared_magic != WEBUI_MAGIC) {
+    printf("Declared file magic number doesn't match the known number: %d/%d\n",
+        declared_magic, WEBUI_MAGIC);
+    retval = 0;
+  }
+
+  fseek(f, 4, SEEK_SET);
+  fread(&declared_checksum, 1, 4, f); // declared checksum
+  checksum = calc_checksum(f);
+  if(declared_checksum != checksum) {
+    printf("Declared checksum doesn't match the real checksum: %#x/%#x\n",
+        declared_checksum, checksum);
+    retval = 1;
+  }
+
+  fseek(f, 8, SEEK_SET);
+  fread(&declared_len, 1, 4, f); // declared file len
+  if(declared_len != file_len) {
+    printf("Declared file size doesn't match the real file size: %d/%d\n",
+        declared_len, file_len);
+    retval = 0;
+  }
+  
+  fseek(f, 12, SEEK_SET);
+  fread(&version, 1, 4, f); // WebUI firmware version
+
+  fseek(f, 16, SEEK_SET); // seek to the first file
+
+  return retval;
+}
+
+int extract_files(FILE *f, const char *dst_path)
+{
+  int   len = 0,
+        max_buf = 0,
+        type = 0;
+  char  file_name[MAX_FILE_NAME],
+        dst_file[MAX_FILE_NAME],
+        *buf = NULL;
+
+  while(!feof(f)) {
+    memset(file_name, 0, sizeof(file_name));
+    memset(dst_file, 0, sizeof(dst_file));
+
+    fread(&len, 1, 4, f); // read filename length
+    fread(file_name,1,len,f); // read filename
+    sprintf(dst_file, "%s%s", dst_path, file_name);
+
+    type = 0;
+    fread(&type, 1, 1, f); // read entry type
+    if (type == 0) { // type: dir
+      if (mkdir(dst_file, 0770) != 0) {
+        fprintf(stderr, "Unable to write file/dir: %s", dst_file);
+        exit(-1);
+      }
+    } else if (type == 1) { // type: file
+      FILE *file = fopen(dst_file, "wb");
+      if (file == NULL) {
+        fprintf(stderr, "Unable to write file: %s", dst_file);
+        exit(-1);
+      }
+
+      fread(&len, 1, 4, f); // read file length
+      if (len > max_buf) {
+        buf = realloc(buf, len);
+        max_buf = len;
+        if (buf == NULL) {
+          fprintf(stderr, "Unable to allocate  data necessary to extract file.  Requested: %d bytes.\n", len);          exit(-1);
+        }
+      }
+      memset(buf, 0, sizeof(len));
+      fread(buf,1,len,f);
+
+      fprintf(stdout, "Extracting %s (%d bytes)...\n", file_name, len);
+      fwrite(buf, 1, len, file);
+      fclose(file);
+    }
+  }
+  free(buf);
   return 1;
 }
 int main(int argc, char **argv) {
@@ -34,9 +130,8 @@ int main(int argc, char **argv) {
   }
   char o;
   int check = 0;
-  int disassemble = 0;
-  char in_file_name[1024] = { 0 };
-  char out_file_name[1024] = { 0 };
+  char in_file_name[MAX_FILE_NAME] = { 0 };
+  char dst_path[MAX_FILE_NAME] = { 0 };
   while((o = getopt(argc, argv, ":c:d:ho:")) != -1) {
     switch (o) {
       case 'c':
@@ -44,14 +139,13 @@ int main(int argc, char **argv) {
         strncpy(in_file_name, optarg, 1023);
         break;
       case 'd':
-        disassemble = 1;
         strncpy(in_file_name, optarg, 1023);
         break;
       case 'h':
         info();
         break;
       case 'o':
-        strncpy(out_file_name, optarg, 1023);
+        strncpy(dst_path, optarg, 1023);
         break;
       case '?':
         printf("Illegal option -%c\n", optopt);
@@ -63,14 +157,21 @@ int main(int argc, char **argv) {
         return -1;
     }
   }
-  printf("in %s out %s\n", in_file_name, out_file_name);
+  
+  if(!strlen(dst_path)) {
+    strncpy(dst_path, DEFAULT_PATH, sizeof(DEFAULT_PATH));
+  }
+
   FILE *file = fopen(in_file_name, "rb");
   if(!file) {
-    printf("Error opnening file %s: %s\n", in_file_name, strerror(errno));
+    printf("Error opening file %s: %s\n", in_file_name, strerror(errno));
     return -1;
   }
-  check_header(file);
-  extract_files(file);
+  if(!check_header(file)) {
+    return -1;
+  }
+  if(!check)
+    extract_files(file, dst_path);
   fclose(file); 
 
 }

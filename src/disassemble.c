5f35a09557281c05b5d8687d3960a3826da7babf
Sergey Shekyan <shekyan@gmail.com>
2013-01-19 21:47:37 -0800

    initial checkin for webui disassembler

--- src/disassemble.c
+++ src/disassemble.c
@@ -3,83 +3,75 @@
  Header goes here 
 
 */
-
-#include <stdio.h>
 #include <dirent.h>
+#include <errno.h>
+#include <getopt.h>
+#include <stdio.h>
 #include <sys/stat.h>
 #include <string.h>
 #include <stdlib.h>
 
-#include "assemble.h"
+#include "disassemble.h"
 
-#define HARDCODED_DIR_NAME "testdir"
-
-int main() {
-  list_target_dir (HARDCODED_DIR_NAME);
-  return 0;
+static void info() {
+  printf("Try \'disassemble -h\' for more information\n"); //, PACKAGE);
 }
 
-char *concatenate (const char* path, const char* fname) {
-  const size_t path_len = strlen(path);
-  const size_t fname_len = strlen(fname);
+int check_header(FILE *f)
+{
+  return 1;
+}
 
-  char *r = (char*) malloc ((path_len + fname_len) * sizeof(*r) + 1);
-  if (r == NULL) {
-    // TODO: verbose error message 
-    // Proper error handling 
-    perror ("malloc failed");
-    exit (-1);
+int extract_files(FILE *f)
+{
+  return 1;
+}
+int main(int argc, char **argv) {
+
+  if (argc < 2) {
+    info();
+    return -1; 
   }
-  
-  r[0] = '\0';
-
-  strcat (r, path);  
-  strcat (r, "/");  
-  strcat (r, fname);
-  return r;
-} 
-
-int list_target_dir (const char* dir_name) { 
-  struct dirent* dir_entry;
-  int err_code = 0;
-
-  // Open the directory 
-  DIR* dir = opendir (dir_name);
-
-  if ( dir == NULL ) {
-    // TODO: verbose error message 
-    // Proper error handling
-    perror ("opendir failed ");
+  char o;
+  int check = 0;
+  int disassemble = 0;
+  char in_file_name[1024] = { 0 };
+  char out_file_name[1024] = { 0 };
+  while((o = getopt(argc, argv, ":c:d:ho:")) != -1) {
+    switch (o) {
+      case 'c':
+        check = 1;
+        strncpy(in_file_name, optarg, 1023);
+        break;
+      case 'd':
+        disassemble = 1;
+        strncpy(in_file_name, optarg, 1023);
+        break;
+      case 'h':
+        info();
+        break;
+      case 'o':
+        strncpy(out_file_name, optarg, 1023);
+        break;
+      case '?':
+        printf("Illegal option -%c\n", optopt);
+        info();
+        return -1; 
+      default:
+        printf("Option -%c requires an argument.\n", optopt);
+        info();
+        return -1;
+    }
+  }
+  printf("in %s out %s\n", in_file_name, out_file_name);
+  FILE *file = fopen(in_file_name, "rb");
+  if(!file) {
+    printf("Error opnening file %s: %s\n", in_file_name, strerror(errno));
     return -1;
   }
-  
-  // Iterate over entries 
-  while ( ( dir_entry = readdir (dir)) != NULL  ) {
-    struct stat s;
+  check_header(file);
+  extract_files(file);
+  fclose(file); 
 
-    // Skip parrent and current directories 
-    if (strcmp(dir_entry->d_name, "..") == 0 || strcmp(dir_entry->d_name, ".") == 0)
-      continue;
-
-    // Read the entr 
-    //if (fstatat (dir_entry, dir_entry->d_name, &s)  != 0) {
-    char *full_path = concatenate (dir_name, dir_entry->d_name); 
-    if (stat ( full_path, &s)) {
-      // TODO: verbose error message 
-      // Proper error handling 
-      perror ("fstatat failed");
-      free(full_path);
-      return -1;
-    }
-
-    if (S_ISDIR(s.st_mode)) {
-      printf ("Found a directory: %s\n", dir_entry->d_name);
-    } 
-    else if (S_ISREG(s.st_mode)) {
-      printf ("Found a regular file: %s\n", dir_entry->d_name);
-    }  
-
-    free(full_path);
-  }
-} 
+}
 
---
+++ src/disassemble.h
@@ -0,0 +1,5 @@
+#ifndef __DISASSEMBLE_H_
+#define __DISASSEMBLE_H_
+
+
+#endif  // __DISASSEMBLE_H_

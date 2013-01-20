70618c4954241170c725ac87d661f9b91e612bab
Artem Harutyunyan <artem@harutyunyan.am>
2013-01-20 12:55:32 -0800

    Adding common.h

---
+++ src/common.h
@@ -0,0 +1,11 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+
+// Constant definitions 
+#define MAX_PATH_LEN 4096
+#define MAX_FILE_NAME_LEN 256 
+#define MAX_FILE_SIZE 1048576
+
+// Function definitions 
+
+#endif  // __COMMON_H

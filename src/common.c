74fe4b206ffcde77dbc8dc430d619d891c05ec1f
Artem Harutyunyan <artem@harutyunyan.am>
2013-01-24 00:49:48 -0800

    Adding common.c

---
+++ src/common.c
@@ -0,0 +1,51 @@
+/*
+ 
+ Header goes here 
+
+*/
+
+#include <stdio.h>
+
+#include "camtool.h"
+#include "common.h"
+
+/*! Takes the open file descriptor pointing to a Web UI file 
+    as an input and calculates the checksum  
+
+    \param f open file descriptor 
+    \return The checksum (sum of all the bytes that follow file header)
+*/ 
+int calc_checksum_file (FILE* f)
+{
+  fseek(f, OFFSET_VERSION, SEEK_SET);
+  int32_t sum = 0;
+  unsigned char buf;
+  while(1){
+    fread(&buf, 1, 1, f);
+    if(feof(f))
+      break;
+    sum += buf;
+  }
+  return sum;
+}
+
+/*! Takes a webui_data_blob populated with the Web UI data as an input
+    and calculates the checksum 
+
+    \param blob the blob containing Web UI data 
+    \return The checksum (sum of all the bytes that follow file header)
+*/ 
+int calc_checksum_blob (const webui_data_blob* blob)
+{
+  int32_t sum = 0;
+  size_t i = 0;
+
+  while (i < blob->size) {
+    sum += blob->data[i];
+    ++i;
+  }
+
+  return sum;
+}
+
+

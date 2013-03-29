eb1c1a1f581f66658cdec215047df7b0e28ad72b
root <root@hartem-VirtualBox.(none)>
2013-03-29 02:00:32 -0700

    ADD: Injecting binaries into the firmware with a single command

--- scripts/commands/get_settings.sh
+++ scripts/commands/get_settings.sh
@@ -5,20 +5,15 @@ export CMD_LIST=("${CMD_LIST[@]}" get_settings)
 
 validate_get_settings()
 {
-    if [[ -z $USERNAME ]] || [[ -z $PASSWORD ]] || [[ -z $ADDR ]]
-    then
-        die "get_settings commands requires username, password and address arguments. See $0 -h for details."
-    fi   
-    
-    CURL=$(which curl)
-    [[ -z $CURL ]] && die "curl not found in \$PATH"
+    validate_admin
+    validate_curl
 }
 
 run_get_settings()
 {
     validate_get_settings
     echo Trying to fetch settings from $ADDR
-    $FILTER $CURL $ADDR/backup_params.cgi'?user='$USERNAME'&pwd='$PASSWORD -o $SETTINGS_FILE >/dev/null 2>&1
+    $CURL $ADDR/backup_params.cgi'?user='$USERNAME'&pwd='$PASSWORD -o $SETTINGS_FILE >/dev/null 2>&1
 }
 
 
